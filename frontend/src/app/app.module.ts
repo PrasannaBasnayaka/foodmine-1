f982ccd880d3995fd30f823c5e56e384ca6a7505
PrasannaBasnayaka <manojbasnayaka72@gmail.com>
2024-01-09 01:05:57 -0800

    foodmine1.1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { StarratingComponent } from './components/partials/starrating/starrating.component';
+import { SearchComponent } from './components/partials/search/search.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    StarratingComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    
   ],
   providers: [
     provideClientHydration()
